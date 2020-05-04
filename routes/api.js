8dd39cfca05ea42aec37b136a7828aa4d77943d6
Darshil Patel <55157259+Darshilp326@users.noreply.github.com>
2020-05-05 01:12:45 +0530

    Added login and signup of patients (#4)
    * Added login and signup for doctors
    
    * login and signup for patients

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,122 @@ const bcrypt=require('bcryptjs');
 const Patient = require('../models/patient');
 const Doctor = require('../models/doctor');
 
-// add a new patient to the db
-router.post('/register', function(req, res, next){
-    Patient.create(req.body).then(function(patient){
-        res.send(patient);
+
+// /patients/signup
+router.post('/patients/signup', (req, res) => {
+  const {email, password, field} = req.body;
+  if ( !email || !password ) {
+      res.status(200).json({ msg: 'Please enter all fields' });
+    }
+  Patient.findOne({ email: email }).then(patient => {
+      
+      if (patient) {
+          return res.status(403).json({ message: "Email is already registered with us." });
+        
+      } else {
+        const newUser = new Patient({
+          email,
+          password,
+          field
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                res.send(user)
+                
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+);
+// patients/signin 
+router.post('/patients/signin',(req,res,next)=>{
+  const {email, password, field} = req.body;
+  Patient.findOne({email:email}).then(patient=>{
+        if(!patient){
+             res.send({message:"No such user exists"});
+          }
+         if(patient){
+           
+          bcrypt.compare(password, patient.password, (err, isMatch) => {
+            if (err) throw err;
+            else if (isMatch) {
+              return res.send(patient);
+            } else {
+              return res.send({ message: 'Password incorrect' });
+            }
+          });
+        }
+   })
+})
+
+// doctors/signup
+router.post('/doctors/signup', (req, res) => {
+    const {email, password, field} = req.body;
+    if ( !email || !password ) {
+        res.status(200).json({ msg: 'Please enter all fields' });
+      }
+    Doctor.findOne({ email: email }).then(doctor => {
+        
+        if (doctor) {
+            return res.status(403).json({ message: "Email is already registered with us." });
+          
+        } else {
+          const newUser = new Doctor({
+            email,
+            password,
+            field
+          });
+  
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+              newUser
+                .save()
+                .then(user => {
+                  res.send(user)
+                  
+                })
+                .catch(err => console.log(err));
+            });
+          });
+        }
+      });
+    }
+  );
+ // doctors/login 
+router.post('/doctors/signin',async(req,res,next)=>{
+    const {email, password, field} = req.body;
+    Doctor.findOne({email:email}).then(doctor=>{
+          if(!doctor){
+               res.send({message:"No such user exists"});
+            }
+           if(doctor){
+             
+            bcrypt.compare(password, doctor.password, (err, isMatch) => {
+              if (err) throw err;
+              else if (isMatch) {
+                return res.send(doctor);
+              } else {
+                return res.send({ message: 'Password incorrect' });
+              }
+            });
+          }
+     })
+})
+// delete a doctor from the db
+router.delete('/doctors/:id', function(req, res, next){
+    Doctor.findByIdAndRemove({_id: req.params.id}).then(function(doctor){
+        res.send(doctor);
     }).catch(next);
 });
 // doctors/signup
