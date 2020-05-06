17167e99565925ab1ab2f8ec8da40ee24abccedc
Darshilp326 <darshil_1901me18@iitp.ac.in>
2020-05-06 23:16:20 +0530

    prescription

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoDB:{
-        dbURL: '<insert_the_url_of_your_db_here>'
+        dbURL: 'mongodb+srv://darshilp326:amisha1974@cluster0-9434z.mongodb.net/test?retryWrites=true&w=majority'
     }
 };
